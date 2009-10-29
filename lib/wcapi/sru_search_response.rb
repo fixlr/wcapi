2d9de5884ded916f7cfe45096c0e1b147cfe22e5
Nathan Fixler <nathan@fixler.org>
2009-10-29 13:05:59 -0400

    Added specs and sample data for SruSearchResponose

--- lib/wcapi/sru_search_response.rb
+++ lib/wcapi/sru_search_response.rb
@@ -1,10 +1,10 @@
 module WCAPI
-  class SruSearchResponse 
+  class SruSearchResponse
     include WCAPI::XPath
     attr_accessor :header, :records, :raw
 
     def initialize(xml)
-      @raw = doc
+      @raw = xml
       @records = []
       @header  = {}
       parse_marcxml(xml)
@@ -20,11 +20,11 @@ module WCAPI
          #likely some kind of xml error
       end
 
-      @header["numberOfRecords"] = xpath_get_text(xpath_first(doc, "//numberOfRecords"))
+      @header["numberOfRecords"] = xpath_get_text(xpath_first(doc, "//numberOfRecords")).to_i
       @header["recordSchema"] = xpath_get_text(xpath_first(doc, "//recordSchema"))
-      @header["nextRecordPosition"] = xpath_get_text(xpath_first(doc, "//nextRecordPosition"))
-      @header["maxiumumRecords"] = xpath_get_text(xpath_first(doc, "//maximumRecords"))
-      @header["startRecord"] = xpath_get_text(xpath_first(doc, "//startRecord"))
+      @header["nextRecordPosition"] = xpath_get_text(xpath_first(doc, "//nextRecordPosition")).to_i
+      @header["maximumRecords"] = xpath_get_text(xpath_first(doc, "//maximumRecords")).to_i
+      @header["startRecord"] = xpath_get_text(xpath_first(doc, "//startRecord")).to_i
  
       nodes = xpath_all(doc, "//records/record/recordData/record")
       nodes.each do |item|
---
+++ spec/wcapi/sru_search_response_spec.rb
@@ -0,0 +1,57 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe WCAPI::SruSearchResponse do
+  shared_examples_for "a WCAPI::SruSearchResponse" do
+    it "should assign a SruSearchResponse" do
+      @response.should be_an_instance_of(WCAPI::SruSearchResponse)
+    end
+    
+    it "should assign an array of records" do
+      @response.records.should be_an_instance_of(Array)
+      @response.records.each do |record|
+        record.should be_an_instance_of(Hash)
+      end
+    end
+  end
+  
+  describe "with an empty query string" do
+    before(:all) do
+      @response = WCAPI::SruSearchResponse.new('')
+    end
+    
+    it_should_behave_like "a WCAPI::SruSearchResponse"
+  end
+  
+  describe "with a sample MARCXML response" do
+    before(:all) do
+      @response = WCAPI::SruSearchResponse.new(File.read(File.dirname(__FILE__)+'/../xml/sru_search_response.xml'))
+      @records = @response.records
+    end
+    
+    it_should_behave_like "a WCAPI::SruSearchResponse"
+    
+    it "should know the numberOfRecords" do
+      @response.header['numberOfRecords'].should == 253773
+    end
+  
+    it "should know the recordSchema" do
+      @response.header['recordSchema'].should == 'info:srw/schema/1/marcxml-v1.1'
+    end
+
+    it "should know the nextRecordPosition" do
+      @response.header['nextRecordPosition'].should == 11
+    end
+
+    it "should know the maximumRecords" do
+      @response.header['maximumRecords'].should == 10
+    end
+
+    it "should have a records array the size of maximumRecords" do
+      @response.header['maximumRecords'].should == @records.size
+    end
+
+    it "should know the startRecord" do
+      @response.header['startRecord'].should == 1
+    end
+  end
+end
