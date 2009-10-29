cf37248e3217dc232b8b7951f4fe491b30d2a413
Nathan Fixler <nathan@fixler.org>
2009-10-29 15:05:58 -0400

    Make sure we're loading the files in this directory, and not wcapi files from a different path

--- lib/wcapi.rb
+++ lib/wcapi.rb
@@ -1,9 +1,6 @@
-require 'wcapi/xpath'
-require 'wcapi/client'
-require 'wcapi/open_search_response'
-require 'wcapi/get_record_response'
-require 'wcapi/get_location_response'
-require 'wcapi/sru_search_response'
+%w(xpath client open_search_response get_record_response get_location_response sru_search_response).each do |file|
+  require File.join(File.dirname(__FILE__), 'wcapi', file)
+end
 
 WORLDCAT_OPENSEARCH = 'http://www.worldcat.org/webservices/catalog/search/opensearch'
 WORLDCAT_SRU = 'http://www.worldcat.org/webservices/catalog/search/sru'
